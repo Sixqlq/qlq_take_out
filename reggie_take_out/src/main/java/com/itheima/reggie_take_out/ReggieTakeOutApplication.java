1b4710000e5fa22449ace54636bd27663180de8d
Sixqlq <2390608028@qq.com>
2022-07-16 05:26:25 +0800

    1. 后台系统登录登出功能 2. 新增员工功能 3. 全局异常处理

--- reggie_take_out/src/main/java/com/itheima/reggie_take_out/ReggieTakeOutApplication.java
+++ reggie_take_out/src/main/java/com/itheima/reggie_take_out/ReggieTakeOutApplication.java
@@ -2,8 +2,10 @@ package com.itheima.reggie_take_out;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieTakeOutApplication {
 
 	public static void main(String[] args) {
--- reggie_take_out/src/main/java/com/itheima/reggie_take_out/controller/EmployeeController.java
+++ reggie_take_out/src/main/java/com/itheima/reggie_take_out/controller/EmployeeController.java
@@ -1,16 +1,19 @@
 package com.itheima.reggie_take_out.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.itheima.reggie_take_out.common.Result;
 import com.itheima.reggie_take_out.entity.Employee;
 import com.itheima.reggie_take_out.service.EmployeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Slf4j
 @RestController
@@ -28,6 +31,70 @@ public class EmployeeController {
      */
     @PostMapping("/login")
     public Result<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
-        return null;
+
+        // 1. 将页面提交的密码password进行MD5加密处理
+        String password = employee.getPassword();
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
+
+        // 2. 根据页面提交的用户名username查询数据库
+        LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        Employee emp = employeeService.getOne(queryWrapper);
+
+        // 3. 如果没有查询到则返回登录失败结果
+        if(emp == null) {
+            return Result.error("登录失败");
+        }
+
+        // 4. 查询到结果则比对密码，如果不一致返回登录失败结果
+        if(!emp.getPassword().equals(password)) {
+            return Result.error("登录失败");
+        }
+
+        // 5. 查看员工状态，如果状态禁用，则返回员工已禁用结果
+        if(emp.getStatus() == 0) {
+            return Result.error("账号已禁用");
+        }
+
+        // 6. 登录成功，将员工id存入session并返回登录成功结果
+        request.getSession().setAttribute("employee", emp.getId());
+        return Result.success(emp);
+    }
+
+    /**
+     * 员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public Result<String> logout(HttpServletRequest request) {
+        // 清理Session中保存的当前登录员工的id
+        request.getSession().removeAttribute("employee");
+        return Result.success("退出成功");
+    }
+
+    /**
+     * 新增员工
+     * @param employee
+     * @return
+     */
+    @PostMapping
+    public Result<String> save(HttpServletRequest request, @RequestBody Employee employee) {
+        log.info("新增员工，员工信息：{}", employee.toString());
+        // 设置初始密码，需要进行MD5加密处理
+        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        // 获得当前登录用户的id
+        Long empId = (Long)request.getSession().getAttribute("employee");
+
+        employee.setCreateUser(empId);
+        employee.setUpdateUser(empId);
+
+        employeeService.save(employee);
+
+        return Result.success("新增员工成功");
     }
 }
