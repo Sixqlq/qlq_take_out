4f65b67d3c6b36db6b841720f8bc72f38c09b1bb
Sixqlq <2390608028@qq.com>
2022-07-15 08:39:12 +0800

    外卖项目搭建环境和框架

---
+++ reggie_take_out/src/main/java/com/itheima/reggie_take_out/controller/EmployeeController.java
@@ -0,0 +1,33 @@
+package com.itheima.reggie_take_out.controller;
+
+import com.itheima.reggie_take_out.common.Result;
+import com.itheima.reggie_take_out.entity.Employee;
+import com.itheima.reggie_take_out.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    /**
+     * 员工登录
+     * @param request
+     * @param employee
+     * @return
+     */
+    @PostMapping("/login")
+    public Result<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+        return null;
+    }
+}
