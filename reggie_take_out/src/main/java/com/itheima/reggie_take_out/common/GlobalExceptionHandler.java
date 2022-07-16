d74702cfd9ab08c9b72967889dcc0dc0b37c1cc4
Sixqlq <2390608028@qq.com>
2022-07-17 04:16:58 +0800

    1. 新增菜品、套餐分类管理功能 2. 新增分类信息分页查询功能 3. 删除分类 4. 修改分类

--- reggie_take_out/src/main/java/com/itheima/reggie_take_out/common/GlobalExceptionHandler.java
+++ reggie_take_out/src/main/java/com/itheima/reggie_take_out/common/GlobalExceptionHandler.java
@@ -31,4 +31,14 @@ public class GlobalExceptionHandler {
         }
         return Result.error("未知错误");
     }
+
+    /**
+     * 异常处理方法
+     * @return
+     */
+    @ExceptionHandler(CustomException.class)
+    public Result<String> exceptionHandler(CustomException exception) {
+        log.error(exception.getMessage());
+        return Result.error(exception.getMessage());
+    }
 }
---
+++ reggie_take_out/src/main/java/com/itheima/reggie_take_out/controller/CategoryController.java
@@ -0,0 +1,79 @@
+package com.itheima.reggie_take_out.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.itheima.reggie_take_out.common.Result;
+import com.itheima.reggie_take_out.entity.Category;
+import com.itheima.reggie_take_out.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 分类管理
+ */
+@RestController
+@RequestMapping("/category")
+@Slf4j
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public Result<String> save(@RequestBody Category category) {
+        log.info("category: {}", category);
+        categoryService.save(category);
+        return Result.success("新增分类成功");
+    }
+
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public Result<Page> page(int page, int pageSize) {
+        // 分页构造器
+        Page<Category> pageInfo = new Page<>(page, pageSize);
+
+        // 条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        // 添加排序条件，根据sort进行排序
+        queryWrapper.orderByAsc(Category::getSort);
+
+        // 进行分页查询
+        categoryService.page(pageInfo, queryWrapper);
+
+        return Result.success(pageInfo);
+    }
+
+    /**
+     * 根据id删除分类
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    public Result<String> delete(Long id) {
+        log.info("删除分类，id为：{}", id);
+        categoryService.remove(id);
+        return Result.success("分类信息删除成功");
+    }
+
+    /**
+     * 根据id修改分类信息
+     * @param category
+     * @return
+     */
+    @PutMapping
+    public Result<String> update(@RequestBody Category category) {
+        log.info("修改分类信息：{}", category);
+        categoryService.updateById(category);
+        return Result.success("修改分类信息成功");
+    }
+}
