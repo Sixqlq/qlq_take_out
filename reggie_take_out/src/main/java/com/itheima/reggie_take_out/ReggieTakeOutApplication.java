eb8cc042206b3ed0a4e582a076db7ef4298d166f
Sixqlq <2390608028@qq.com>
2022-07-21 02:41:45 +0800

    1. 菜品数据缓存到redis 2. 使用Spring Cache 缓存套餐数据

--- reggie_take_out/src/main/java/com/itheima/reggie_take_out/ReggieTakeOutApplication.java
+++ reggie_take_out/src/main/java/com/itheima/reggie_take_out/ReggieTakeOutApplication.java
@@ -3,11 +3,13 @@ package com.itheima.reggie_take_out;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @ServletComponentScan
 @EnableTransactionManagement
+@EnableCaching // 开启Spring Cache 注解方式缓存功能
 public class ReggieTakeOutApplication {
 
 	public static void main(String[] args) {
